135dd11ca93d1e06635c2493f2c8d7d5e0160b68
a-bigelow <adam@adambigelow.com>
2022-01-09 15:27:42 -0500

    chore(colors): resolving colors version because of random internet package drama

--- .projenrc.js
+++ .projenrc.js
@@ -24,5 +24,6 @@ const project = new awscdk.AwsCdkConstructLibrary({
 });
 project.setScript('format', 'prettier -w "src/**/*.ts"');
 project.setScript('commit', 'cz');
-project.gitignore.addPatterns('.idea/')
+project.gitignore.addPatterns('.idea/');
+project.addFields( {'resolutions': { "colors": "1.4.0" }})
 project.synth();
