6a5a3b184b17eead5e5351852c0a32f3df27fddc
a-bigelow <adam@adambigelow.com>
2022-01-08 19:22:58 -0500

    initial prototype

--- .projenrc.js
+++ .projenrc.js
@@ -1,15 +1,26 @@
-const { awscdk } = require('projen');
+const { awscdk } = require("projen");
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'a-bigelow',
-  authorAddress: 'adam@adambigelow.com',
-  cdkVersion: '2.1.0',
-  defaultReleaseBranch: 'main',
-  name: 'satisfactory-server-cdk',
-  repositoryUrl: 'https://github.com/adam/satisfactory-server-cdk.git',
+  author: "Adam Bigelow",
+  authorAddress: "adam@adambigelow.com",
+  cdkVersion: "2.4.0",
+  defaultReleaseBranch: "main",
+  name: "satisfactory-server-cdk",
+  repositoryUrl: "https://github.com/a-bigelow/satisfactory-server-cdk.git",
+  eslint: false,
+  prettier: true,
+  prettierOptions: {
+    settings: {
+      useTabs: true,
+      tabWidth: 4,
+      printWidth: 120,
+    },
+  },
+  release: false,
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
+project.setScript('format', 'prettier -w "src/**/*.ts"');
 project.synth();
